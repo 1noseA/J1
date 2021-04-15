afe40feafa2d925390f0555ee6ba6f4332f5029e
1noseA <1noseA2L@gmail.com>
2021-04-15 23:25:22 +0900

    [JavaSilver]API_Comparator

---
+++ src/silver9_2/Sample.java
@@ -0,0 +1,20 @@
+package silver9_2;
+
+public class Sample {
+	private int id;
+	private String name;
+
+	public Sample(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
