cea6db51161d9b23eab33fc1ffbe37f5d0be4d36
1noseA <1noseA2L@gmail.com>
2021-04-15 22:43:30 +0900

    [JavaSilver]例外処理

---
+++ src/silver/reigai10.java
@@ -0,0 +1,38 @@
+package silver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class reigai10 {
+	public static void main(String[] args) {
+		// 従来の書き方
+//		FileInputStream is = null;
+//		try {
+//			is = new FileInputStream("sample.txt");
+//			throw new FileNotFoundException();
+//		} catch (Exception e) {
+//			System.out.println("A");
+//		} finally {
+//			if (is != null) {
+//				try {
+//					is.close();
+//				} catch (IOException e) {
+//					throw new RuntimeException();
+//				}
+//			}
+//			System.out.println("B");
+//		}
+
+		try (FileInputStream is = new FileInputStream("sample.txt");) {
+			throw new FileNotFoundException();
+		} catch (Exception e) {
+			System.out.println("A");
+		} finally {
+			// tryブロックで宣言した変数の有効範囲はtryブロックの中だけ。コンパイルエラー
+//			if (is != null) {
+//				is.close();
+//			}
+			System.out.println("B");
+		}
+	}
+}
