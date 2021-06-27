a66796260aa661bcc6c597c9686e1224680ae963
1noseA <1noseA2L@gmail.com>
2021-06-27 16:51:08 +0900

    hashCodeメソッドと可変長引数

--- src/j20210602/Item.java
+++ src/j20210602/Item.java
@@ -1,5 +1,7 @@
 package j20210602;
 
+import java.util.Objects;
+
 public class Item {
 	private String name;
 	private int price;
@@ -19,52 +21,57 @@ public class Item {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + price;
-		return result;
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+
+		// Item型と互換性のある型かどうかを確認
+		// 互換性がない場合
+		if (obj instanceof Item == false) {
+			return false;
+		}
+
+		// 互換性があるから型変換をする(objをItemniキャスト)
+		Item target = (Item) obj;
+		return this.name.equals(target.name);
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+
+//	// 全てのフィールド突合したい場合は自動生成できる
 //	@Override
 //	public boolean equals(Object obj) {
-//		if (obj == null) {
+//		if (this == obj)
+//			return true;
+//		// 参照先がないかどうか判定
+//		if (obj == null)
 //			return false;
-//		}
-//
-//		// Item型と互換性のある型かどうかを確認
-//		// 互換性がない場合
-//		if (obj instanceof Item == false) {
+//		// 同じクラスかどうか判定
+//		if (getClass() != obj.getClass())
 //			return false;
-//		}
-//
-//		// 互換性があるから型変換をする(objをItemniキャスト)
-//		Item target = (Item) obj;
-//		return this.name.equals(target.name);
+//		// ダウンキャスト
+//		Item other = (Item) obj;
+//		// 名前がnullでなかったら相手の名前と比較する
+//		if (name == null) {
+//			if (other.name != null)
+//				return false;
+//		} else if (!name.equals(other.name))
+//			return false;
+//		if (price != other.price)
+//			return false;
+//		return true;
 //	}
-
-	// 全てのフィールド突合したい場合は自動生成できる
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		// 参照先がないかどうか判定
-		if (obj == null)
-			return false;
-		// 同じクラスかどうか判定
-		if (getClass() != obj.getClass())
-			return false;
-		// ダウンキャスト
-		Item other = (Item) obj;
-		// 名前がnullでなかったら相手の名前と比較する
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (price != other.price)
-			return false;
-		return true;
-	}
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((name == null) ? 0 : name.hashCode());
+//		result = prime * result + price;
+//		return result;
+//	}
 }
--- src/j20210602/Sample.java
+++ src/j20210602/Sample.java
@@ -10,5 +10,28 @@ public class Sample {
 
 		System.out.println(a == b);
 		System.out.println(a.equals(b));
+
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+
+		// 引数をいくつでも渡せる（配列に置き換わる）
+//		test(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+		// ->
+		test(new int[] {1, 2, 3, 4, 5});
+		// 上記は無名配列。この省略系
+		// int[] array = {1, 2, 3, 4, 5};
+		// test(array[]);
+	}
+
+	// 可変長引数
+//	public static void test(int[] array) {
+//		for (int n : array) {
+//			System.out.println(n);
+//		}
+//	}
+	public static void test(int... array) {
+		for (int n : array) {
+			System.out.println(n);
+		}
 	}
 }
