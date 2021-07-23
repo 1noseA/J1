a3844ab8d6dfe6210e2cefbd24d5fa247ad5bfcc
1noseA <1noseA2L@gmail.com>
2021-07-23 16:04:43 +0900

    20210707_Map

--- src/j20210707/Sample.java
+++ src/j20210707/Sample.java
@@ -1,6 +1,7 @@
 package j20210707;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Sample {
@@ -14,11 +15,48 @@ public class Sample {
 //			System.out.println(str);
 //		}
 
-		Set<Item> set = new HashSet<>();
-		set.add(new Item("apple", 100));
-		set.add(new Item("banana", 80));
-		set.add(new Item("orange", 120));
+//		Set<Item> set = new HashSet<>();
+//		set.add(new Item("apple", 100));
+//		set.add(new Item("banana", 80));
+//		set.add(new Item("orange", 120));
+//
+//		System.out.println(set.size());
 
-		System.out.println(set.size());
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		// 上書きされる
+		map.put(1, "hello");
+		// valueは一意でなくてよい（"one"はコンスタントプールで同じリンクへの参照になる）
+		map.put(4, "one");
+		// nullも使える
+		map.put(null, null);
+
+		Set<Integer> keys = map.keySet();
+		for (Integer key : keys) {
+			System.out.println(map.get(key));
+		}
+
+		// 補足
+		Item apple = new Item("apple", 100);
+		Item orange = new Item("orange", 120);
+		Item banana = new Item("banana", 80);
+
+		Map<String, Item> itemMap = new HashMap<>();
+		itemMap.put(apple.getName(), apple);
+		itemMap.put(orange.getName(), orange);
+		itemMap.put(banana.getName(), banana);
+
+		Set<String> keys2 = itemMap.keySet();
+		// どのクラスが動いているのか調べる
+		System.out.println(keys2.getClass().getName());
+
+		Set<Map.Entry<String, Item>> set = itemMap.entrySet();
+		for (Map.Entry<String, Item> entry : set) {
+			System.out.println(entry.getKey());
+			System.out.print(":");
+			System.out.println(entry.getValue());
+		}
 	}
 }
