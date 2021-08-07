c6d5f64a349f84971d025352dd0284bbb268b274
1noseA <1noseA2L@gmail.com>
2021-08-07 22:57:03 +0900

    追加

--- src/j20210804/Sample2.java
+++ src/j20210804/Sample2.java
@@ -5,54 +5,12 @@ import java.util.List;
 
 public class Sample2 {
 	public static void main(String[] args) {
-		// ローカルクラス
-//		class Test {
-//			private String value;
-//		}
-//
-//		Test t = new Test();
-//		t.value = "hoge";
-
-		// 無名クラス
-//		List<String> list = new List() {
-//
-//		};
-
-//		List<String> list = new ArrayList() {
-//			@Override
-//			public boolean add(Object e) {
-//				System.out.println("hello");
-//				return super.add(e);
-//			}
-//		};
-//		// addするとコンソールには「hello」と出る
-//		list.add("hoge");
-
-		// ちなみにクラス名を調べると
-		// System.out.println(list.getClass().getName());
-
 		List<String> list = new ArrayList() {
-			// 初期化ブロック
-			{
-				super.add("A");
-				super.add("B");
-				super.add("C");
+			@Override
+			public boolean add(Object e) {
+				System.out.println("hello");
+				return super.add(e);
 			}
 		};
-
-		// メソッドの引数を書ける
-		test(new ArrayList() {
-			{
-				super.add("A");
-				super.add("B");
-				super.add("C");
-			}
-		});
-	}
-
-	public static void test(List list) {
-		for (Object obj : list) {
-			System.out.println(obj);
-		}
 	}
 }
