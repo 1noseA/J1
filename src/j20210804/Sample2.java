cbf61cee41e61b417c769a6c100598c5ca357c8d
1noseA <1noseA2L@gmail.com>
2021-08-07 19:39:47 +0900

    ローカルクラス

---
+++ src/j20210804/Sample2.java
@@ -0,0 +1,12 @@
+package j20210804;
+
+public class Sample2 {
+	public static void main(String[] args) {
+		class Test {
+			private String value;
+		}
+
+		Test t = new Test();
+		t.value = "hoge";
+	}
+}
