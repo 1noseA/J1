78b2da5d4e5816ad7ec558e9eb755c03e48a2551
1noseA <1noseA2L@gmail.com>
2021-08-07 20:22:13 +0900

    初期化ブロック

--- src/j20210804/Sample2.java
+++ src/j20210804/Sample2.java
@@ -18,17 +18,41 @@ public class Sample2 {
 //
 //		};
 
-		List<String> list = new ArrayList() {
-			@Override
-			public boolean add(Object e) {
-				System.out.println("hello");
-				return super.add(e);
-			}
-		};
-		// addするとコンソールには「hello」と出る
-		list.add("hoge");
+//		List<String> list = new ArrayList() {
+//			@Override
+//			public boolean add(Object e) {
+//				System.out.println("hello");
+//				return super.add(e);
+//			}
+//		};
+//		// addするとコンソールには「hello」と出る
+//		list.add("hoge");
 
 		// ちなみにクラス名を調べると
 		// System.out.println(list.getClass().getName());
+
+		List<String> list = new ArrayList() {
+			// 初期化ブロック
+			{
+				super.add("A");
+				super.add("B");
+				super.add("C");
+			}
+		};
+
+		// メソッドの引数を書ける
+		test(new ArrayList() {
+			{
+				super.add("A");
+				super.add("B");
+				super.add("C");
+			}
+		});
+	}
+
+	public static void test(List list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
