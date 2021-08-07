c5c31451c78bf31a78fffe38d46247b0dd9c6292
1noseA <1noseA2L@gmail.com>
2021-08-07 20:04:42 +0900

    無名クラス

--- src/j20210804/Sample2.java
+++ src/j20210804/Sample2.java
@@ -1,12 +1,34 @@
 package j20210804;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sample2 {
 	public static void main(String[] args) {
-		class Test {
-			private String value;
-		}
+		// ローカルクラス
+//		class Test {
+//			private String value;
+//		}
+//
+//		Test t = new Test();
+//		t.value = "hoge";
 
-		Test t = new Test();
-		t.value = "hoge";
+		// 無名クラス
+//		List<String> list = new List() {
+//
+//		};
+
+		List<String> list = new ArrayList() {
+			@Override
+			public boolean add(Object e) {
+				System.out.println("hello");
+				return super.add(e);
+			}
+		};
+		// addするとコンソールには「hello」と出る
+		list.add("hoge");
+
+		// ちなみにクラス名を調べると
+		// System.out.println(list.getClass().getName());
 	}
 }
