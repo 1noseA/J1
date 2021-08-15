dcc18ca1687a4070b644ee50d7c7f7b8cb3a26fd
1noseA <1noseA2L@gmail.com>
2021-08-15 17:51:15 +0900

    Stream

--- src/j20210804/Sample6.java
+++ src/j20210804/Sample6.java
@@ -1,17 +1,51 @@
 package j20210804;
 
-import java.util.function.Supplier;
+import java.util.List;
 
 public class Sample6 {
 	public static void main(String[] args) {
-		Supplier<Hello> factory = () -> {
-			return new HelloImpl();
-		};
-		test(factory);
+		// factoryクラスと同じように使える
+//		Supplier<Hello> factory = () -> {
+//			return new HelloImpl();
+//		};
+//		test(factory);
+
+		List<Item> list = List.of(
+				new Item("apple", 100),
+				new Item("orange", 120),
+				new Item("banana", 200)
+				);
+
+		// 引数を受け取って条件に一致するかどうか判定する
+//		Predicate<Item> p = (item) -> {
+//			return item.getPrice() > 100;
+//		};
+
+		// 出力するときにフィルタリングできる
+		// list.stream…一覧に並べて1つずつ送り出してくれる
+		// 100より上だったらtrue下だったらfalse
+		// trueのものだけがforEach内のprintlnメソッド実行される
+//		list.stream()
+//			.filter(p)
+//			.forEach(System.out::println);
+
+		// 以下を簡単に書いたもの
+//		for (Item item : list) {
+//			if (item.getPrice() > 100) {
+//				System.out.println(item);
+//			}
+//		}
+
+		// こんな書き方もできる
+		list.stream()
+		.filter((item) -> {
+			return item.getPrice() > 100;
+		})
+		.forEach(System.out::println);
 	}
 
-	private static void test(Supplier<Hello> factory) {
-		Hello h = factory.get();
-		h.hoge();
-	}
+//	private static void test(Supplier<Hello> factory) {
+//		Hello h = factory.get();
+//		h.hoge();
+//	}
 }
