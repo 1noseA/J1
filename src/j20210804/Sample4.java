49b79db99f4b907788d3d9aec03d7031f7754edf
1noseA <1noseA2L@gmail.com>
2021-08-15 12:38:41 +0900

    メソッド参照

--- src/j20210804/Sample4.java
+++ src/j20210804/Sample4.java
@@ -1,7 +1,6 @@
 package j20210804;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public class Sample4 {
 	public static void main(String[] args) {
@@ -11,14 +10,27 @@ public class Sample4 {
 				);
 		// <>内にデータ型が書いてあるので、()内のデータ型は省略できる
 		// 定義しているのであって実行していない
-		Consumer<String> c = (str) -> {
-			System.out.println(str);
-		};
+//		Consumer<String> c = (str) -> {
+//			System.out.println(str);
+//		};
+
+		// メソッド参照
+		// Consumer<String> c = Sample4::test;
+
+		// さらに
+		// Consumer<String> c = System.out::println;
 
 		// 実行(遅延実行という)
 		// c.accept("hoge");
 
 		// ラムダ式と同時に導入された便利なメソッド(繰り返し文)
-		list.forEach(c);
+		// list.forEach(c);
+
+		// 直接メソッドを渡せる
+		list.forEach(System.out::println);
+	}
+
+	private static void test(String str) {
+		System.out.println(str);
 	}
 }
