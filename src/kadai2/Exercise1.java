f196c969b30a6eebf8c11d2c71a37ccaf4bf8b03
1noseA <1noseA2L@gmail.com>
2021-08-22 17:13:05 +0900

    演習2(1-1)

---
+++ src/kadai2/Exercise1.java
@@ -0,0 +1,9 @@
+package kadai2;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+        Section section = new Section();
+        section.setName("人事部");
+        System.out.println(section.getName());
+    }
+}
