c965bffaae3acf850d852bc31f79fb178da91e38
1noseA <1noseA2L@gmail.com>
2021-08-22 17:22:17 +0900

    演習2(1-3)

--- src/kadai2/Exercise1.java
+++ src/kadai2/Exercise1.java
@@ -2,9 +2,15 @@ package kadai2;
 
 public class Exercise1 {
 	public static void main(String[] args) {
-        Section section = new Section();
-        section.setName("人事部");
-        // System.out.println(section.getName());
-        section.printName(section.getName());
+        Section section1 = new Section();
+        Section section2 = new Section();
+        Section section3 = new Section();
+        section1.setName("人事部");
+        section2.setName("営業部");
+        section3.setName("技術部");
+
+        section1.printName(section1.getName());
+        section2.printName(section2.getName());
+        section3.printName(section3.getName());
     }
 }
